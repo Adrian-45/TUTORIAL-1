f2d11b55a0d3e88155019d3c919dd73d168bb14a
Adrian-45 <divyanshjadav2008@gmail.com>
2026-06-20 17:06:37 +0530

    renaming and adding chapter 3 ps

---
+++ chapter 3/02template.py
@@ -0,0 +1,13 @@
+print ('I can generate a letter for you')
+a = input ('please give us the name')
+b = input ('please give us the date')
+
+print ("dear",a, "\n you are selected" "\n",b,)
+# can be made better using fstring
+
+###letter = '''Dear <|Name|>, 
+# You are selected! 
+# <|Date|> '''
+
+# print(letter.replace("<|Name|>", "Harry").replace("<|Date|", "24 September 2050"))
+
